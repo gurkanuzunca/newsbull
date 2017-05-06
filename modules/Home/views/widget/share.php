138c5ac61ee14a7727a5cb6b5385a6dd59b54da8
Gurkan Uzunca <selam@gurkanuzunca.com>
2017-05-06 13:31:14 +0300

    Issue #11 closed. Sosyal medya paylaşımı popup.

--- modules/Home/views/widget/share.php
+++ modules/Home/views/widget/share.php
@@ -1,7 +1,7 @@
 <div class="common-buttons">
-    <a class="btn facebook" href="https://facebook.com/sharer.php?u=<?php echo current_url() ?>" title="<?php echo lang('Facebook\'ta Paylaş') ?>"><i class="fa fa-facebook"></i> <?php echo lang('Facebook\'ta Paylaş') ?></a>
-    <a class="btn twitter" href="https://twitter.com/share?url=<?php echo current_url() ?>&text=<?php echo htmlspecialchars($record->title) ?>" title="<?php echo lang('Twitter\'da Paylaş') ?>"><i class="fa fa-twitter"></i> <?php echo lang('Twitter\'da Paylaş') ?></a>
-    <a class="btn google" href="https://plus.google.com/share?url=<?php echo current_url() ?>" title="<?php echo lang('Google+\'da Paylaş') ?>"><i class="fa fa-google-plus"></i> <?php echo lang('Google+\'da Paylaş') ?></a>
+    <a class="btn facebook share-box" href="https://facebook.com/sharer.php?u=<?php echo current_url() ?>" title="<?php echo lang('Facebook\'ta Paylaş') ?>"><i class="fa fa-facebook"></i> <?php echo lang('Facebook\'ta Paylaş') ?></a>
+    <a class="btn twitter share-box" href="https://twitter.com/share?url=<?php echo current_url() ?>&text=<?php echo htmlspecialchars($record->title) ?>" title="<?php echo lang('Twitter\'da Paylaş') ?>"><i class="fa fa-twitter"></i> <?php echo lang('Twitter\'da Paylaş') ?></a>
+    <a class="btn google share-box" href="https://plus.google.com/share?url=<?php echo current_url() ?>" title="<?php echo lang('Google+\'da Paylaş') ?>"><i class="fa fa-google-plus"></i> <?php echo lang('Google+\'da Paylaş') ?></a>
     <?php if (isset($record->visited)): ?>
         <span class="btn visited"><?php echo lang('Görüntülenme'); ?>: <strong><?php echo $record->visited ?></strong></span>
     <?php endif; ?>

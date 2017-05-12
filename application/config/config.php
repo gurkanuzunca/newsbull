5099e85b8056e6dd7b0c2bd4612bfadfae7d446f
Gurkan Uzunca <selam@gurkanuzunca.com>
2017-05-12 22:22:07 +0300

    Csrf config

--- application/config/config.php
+++ application/config/config.php
@@ -442,11 +442,11 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
 $config['csrf_protection'] = FALSE;
-$config['csrf_token_name'] = 'csrf_test_name';
-$config['csrf_cookie_name'] = 'csrf_cookie_name';
+$config['csrf_token_name'] = 'newsbullToken';
+$config['csrf_cookie_name'] = 'newsbullToken';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
-$config['csrf_exclude_uris'] = array();
+$config['csrf_exclude_uris'] = array('admin/.*');
 
 /*
 |--------------------------------------------------------------------------

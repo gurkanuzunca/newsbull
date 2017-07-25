ae272340a9061ebd706445c502470901edf29882
Gurkan Uzunca <selam@gurkanuzunca.com>
2017-07-25 20:23:31 +0300

    git igniore production

--- application/config/production/database.php
+++ application/config/production/database.php
@@ -73,6 +73,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
